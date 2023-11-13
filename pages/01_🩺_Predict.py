03cfde156bc15eb859d7bd8f2c1b3a6ad4634987
“Tobias” <tobi_m@live.dk>
2023-11-13 18:43:18 -0500

    Basics done. Pred model is Log Reg

--- pages/01_🩺_Predict.py
+++ pages/01_🩺_Predict.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from joblib import load
 import numpy as np
+import pandas as pd
 
 st.set_page_config(
     page_title = "Predict", 
@@ -8,12 +9,29 @@ st.set_page_config(
     layout = "centered"
 )
 
-def load_model_and_scaler():
-    """Loads the logistic regression model and scaler from joblib files."""
-    logreg = load('logreg_model.joblib')
-    scaler = load('scaler.joblib')
-    
-    return logreg, scaler
+def calculate_bmi_pounds_feet_inches(weight_pounds, height_feet, height_inches):
+     total_inches = (height_feet * 12) + height_inches
+     height_m = total_inches * 0.0254
+     weight_kg = weight_pounds * 0.453592
+     if height_m == 0:
+         return "Invalid height"
+     bmi = weight_kg / (height_m ** 2)
+     return bmi
+ 
+def calculate_bmi_metric(weight_kg, height_cm):
+     height_m = height_cm / 100  # Convert cm to meters
+     if height_m == 0:
+         return "Invalid height"
+     bmi = weight_kg / (height_m ** 2)
+     return bmi
+
+# Function to load your logistic regression model and preprocessor
+def load_model_and_preprocessor():
+    model = joblib.load('logreg_model.joblib')
+    preprocessor = joblib.load('preprocessor.joblib')
+    return model, preprocessor
+
+
 
 def predict_diabetes(logreg, scaler, user_input):
     """Predicts diabetes based on user input using the given model and scaler."""
@@ -21,81 +39,165 @@ def predict_diabetes(logreg, scaler, user_input):
     prediction = logreg.predict(X_scaled)
     return prediction
 
+
 def show_predict_page():
     st.title("Do you have diabetes or not?  :mending_heart: :hospital:")
 
     st.write("""### We need some information to make a prediction""")
     
     # User input fields
-    # Define a dictionary for gender mapping
-    gender_mapping = {
-        "Male": 1,
-        "Female": 2
-    }
+    st.subheader('Demographic Information 👤')
 
-    # Let user select using the human-readable options
-    selected_gender = st.selectbox(
-        label = "What gender were you assigned at birth?", 
-        options = list(gender_mapping.keys()),
+    # Let user select gender using the human-readable options
+    Gender = st.selectbox(
+        label = "What gender were you assigned at birth?",
+        options = ["Male", "Female"],  # Assuming these were the original categories
         index = None,
         placeholder = "Choose a response...")
 
-    # Convert the selected option to its numerical value
-    gender = gender_mapping.get(selected_gender)
+    # Age
+    Age_Years = st.slider('How old are you?', 0, 80, 25)
 
-    hip_circumference = st.number_input(
+    # Race
+    Race = st.selectbox(
+        label = "What race/ethnicity best describes you?",
+        options = ["Mexican American", "Other Hispanic", "Non-Hispanic White", 
+                "Non-Hispanic Black", "Non-Hispanic Asian", 
+                "Other Race - Including Multi-Racial"],  # Assuming these were the original categories
+        index = None,
+        placeholder = "Choose a response...")
+
+
+    st.subheader('Body Measurements 📐')
+ 
+    # User selects the unit system
+    unit_system = st.radio("Select the unit system", ['Imperial (pounds, feet, inches)', 'Metric (kilograms, centimeters)'])
+
+    if unit_system == 'Imperial (pounds, feet, inches)':
+        weight_pounds = st.number_input('Enter your weight (in pounds):', min_value=1.0, max_value=600.0, step=0.1, value=1.0, placeholder="Type a number...")
+        height_feet = st.number_input('Enter your height (feet):', min_value=0, max_value=7, step=1, value=1, placeholder="Type a number...")
+        height_inches = st.number_input('Enter your height (additional inches):', min_value=0, max_value=11, step=1, value=1, placeholder="Type a number...")
+        BMI_Value = calculate_bmi_pounds_feet_inches(weight_pounds, height_feet, height_inches)
+    elif unit_system == 'Metric (kilograms, centimeters)':
+        weight_kg = st.number_input('Enter your weight (in kilograms):', min_value=1.0, max_value=300.0, step=0.1, value=1.0, placeholder="Type a number...")
+        height_cm = st.number_input('Enter your height (in centimeters):', min_value=50, max_value=250, step=1,  value=50, placeholder="Type a number...")
+        BMI_Value = calculate_bmi_metric(weight_kg, height_cm)
+    
+    Hip_Circumference_cm = st.number_input(
         label = "Hip Circumference (cm)", 
         value = None,
         step = 1,
         placeholder = "Type a number...")
     
-    moderate_work_activity_mapping = {
-        "Yes": 1, 
-        "No": 2,
-    }
+    Waist_Circumference_cm = st.number_input(
+        label = "Waist Circumference (cm)", 
+        value = None,
+        step = 1,
+        placeholder = "Type a number...")
 
-    # Let user select using the human-readable options
-    selected_moderate_work_activity = st.selectbox(
-        label = "Does your work involve moderate-intensity activity that causes small increases in breathing or heart rate such as brisk walking or carrying light loads for at least 10 minutes continuously?", 
-        options = list(moderate_work_activity_mapping.keys()),
+    st.subheader('Lifestyle 🏄‍♂️🍽️')
+    
+    # Let user select using the human-readable options    
+    Vigorous_work_activity = st.selectbox(
+        label = "Does your work involve moderate-intensity activity that causes small increases in breathing or heart rate such as brisk walking or carrying light loads for at least 10 minutes continuously?",
+        options = ["Yes", "No"],
         index = None,
         placeholder = "Choose a response...")
     
-    moderate_work_activity = moderate_work_activity_mapping.get(selected_moderate_work_activity)
+    Walk_or_bicycle = st.selectbox(
+        label="In a typical week, do you walk or use a bicycle for at least 10 minutes continuously to get to and from places?",
+        options=["Yes", "No"],  # Assuming these were the original categories
+        index = None,
+        placeholder="Choose a response...")
+    
+    Vigorous_recreational_activities = st.selectbox(
+        label = "Do you engage in any vigorous sports, fitness, or recreational activities like running or basketball that significantly elevate your breathing or heart rate for at least 10 minutes continuously in a typical week?",
+        options = ["Yes", "No"],
+        index = None,
+        placeholder = "Choose a response...")
+    
+    Meals_Outside = st.number_input(
+        label = "In the past 7 days, how many meals did you get that were prepared away from home in places such as restaurants, fast food places, food stands, grocery stores, or from vending machines?",
+        min_value = 0,
+        value = None,
+        step = 1,
+        placeholder = "Type a number...")
+
+    FastFood_Meals = st.number_input(
+        label = "Continuation of last question: How many of those meals did you get from a fast-food or pizza place?",
+        min_value = 0,
+        value = None,
+        step = 1,
+        placeholder = "Type a number...")
+    
+    Ready_Eat_Foods_30D = st.number_input(
+        label = "How often in the past 30 days did you eat \"ready to eat\" foods like salads, soups, or sandwiches from a grocery store's salad bar or deli counter?",
+        min_value = 0,
+        value = None,
+        step = 1,
+        placeholder = "Type a number...")
+    
+    Frozen_Meals_30D = st.number_input(
+        label = "In the past 30 days, how often did you eat frozen meals or frozen pizzas?",
+        min_value = 0,
+        value = None,
+        step = 1,
+        placeholder = "Type a number...")
+    
+    Past_12_Mo_Alco = st.select_slider(
+        label="How frequently have you drunk alcoholic beverages in the past 12 months",
+        options=[
+            "Every day", 
+            "Nearly every day", "3 to 4 times a week", 
+            "2 times a week",
+            "Once a week", 
+            "2 to 3 times a month", "Once a month",
+            "7 to 11 times in the last year", 
+            "3 to 6 times in the last year",
+            "1 to 2 times in the last year", 
+            "Never in the last year"])
 
     # Button to predict
     ok = st.button("Predict")
 
     if ok:
-        if gender is None or moderate_work_activity is None or hip_circumference is None:
+        if (Past_12_Mo_Alco is None or FastFood_Meals is None or Meals_Outside is None or Vigorous_recreational_activities is None 
+            or Walk_or_bicycle is None or Vigorous_work_activity is None or Waist_Circumference_cm is None 
+            or Hip_Circumference_cm is None or Race is None or Gender is None):
+
+
             st.warning("Please make valid selections and provide hip circumference before predicting.")
         else:
-            # Create a numpy array from the user input
-            user_input = np.array([[gender, hip_circumference, moderate_work_activity]])
-            user_input = user_input.astype(float)
-
-            # Load model and scaler
-            logreg, scaler = load_model_and_scaler()
+                # Create a DataFrame from the user input
+            user_input_df = pd.DataFrame([[Meals_Outside, FastFood_Meals, Ready_Eat_Foods_30D, 
+                                           Frozen_Meals_30D, Gender, Age_Years, 
+                                           Race, Vigorous_work_activity, Walk_or_bicycle, Vigorous_recreational_activities, 
+                                           BMI_Value, Waist_Circumference_cm, Hip_Circumference_cm, Past_12_Mo_Alco]], 
+                                           
+                                           columns=['Meals_Outside', 'FastFood_Meals', 'Ready_Eat_Foods_30D', 
+                                                    'Frozen_Meals_30D', 'Gender', 'Age_Years', 
+                                                    'Race', 'Vigorous_work_activity', 'Walk_or_bicycle', 'Vigorous_recreational_activities', 
+                                                    'BMI_Value', 'Waist_Circumference_cm', 'Hip_Circumference_cm', 'Past_12_Mo_Alco'])
             
+            # Load the logistic regression model
+            logreg = load('logistic_regression_model.joblib')
+
+            # Load the preprocessor
+            preprocessor = load('preprocessor.joblib')
+
+            # Preprocess the user input
+            user_input_preprocessed = preprocessor.transform(user_input_df)
+
             # Make prediction
-            prediction = predict_diabetes(logreg, scaler, user_input)
-            
-            # Mapping of model outputs to desired strings
-            output_mapping = {
-                0: ":smiley: Our predictions suggests you may not have diabetes",
-                1: ":pensive: Our prediction says you may have diabetes. You can read more about diabetes on [CDC's website](https://www.cdc.gov/diabetes/basics/symptoms.html)"
-            }
-            
-            # Get the corresponding string for the prediction
-            predicted_string = output_mapping.get(prediction[0], "Unknown")
+            prediction = logreg.predict(user_input_preprocessed)
 
-            # Display the mapped prediction string using appropriate streamlit function
+            
+            # Display the prediction
             if prediction[0] == 1:
-                st.error(predicted_string)
-            elif prediction[0] == 0:
-                st.success(predicted_string)
+                st.error(":pensive: Our prediction says you may have diabetes. You can read more about diabetes on [CDC's website](https://www.cdc.gov/diabetes/basics/symptoms.html)")
             else:
-                st.subheader(predicted_string)  # Default for any other unexpected values
+                st.success(":smiley: Our predictions suggest you may not have diabetes")
+
 
 show_predict_page()
  
--- 🏡_Homepage.py
+++ 🏡_Homepage.py
@@ -10,16 +10,23 @@ st.title("Welcome to our MDST Project! 👋")
 
 def show_homepage():
 
-    st.subheader("Discover Your Diabetes Risk with a Click! :mag:")
+    st.subheader("Assess Your Diabetes Risk by Taking Our Survey! 📝")
     st.markdown("""
-    This application offers users the chance to complete a brief survey. Based on your responses, we predict the likelihood of you having diabetes. Our prediction model is trained using data sourced from NHANES.
+    This application offers users the chance to complete a brief survey. Based on your responses, we predict the likelihood of you having diabetes - check it out in the sidebar. Our prediction model is trained using data sourced from NHANES.
     
     **What is NHANES?**  
-    The National Health and Nutrition Examination Survey (NHANES) is an initiative designed to gauge the health and nutritional status of adults and children across the United States. Through this comprehensive survey, valuable insights into various health concerns are gathered and analyzed, making it an invaluable resource for predictive models like ours.
-    """)
+                The National Health and Nutrition Examination Survey (NHANES) is an initiative designed to gauge the health and nutritional status of adults and children across the United States. Through this comprehensive survey, valuable insights into various health concerns are gathered and analyzed, making it an invaluable resource for predictive models like ours.
     
-    st.header("Reference")
-    st.markdown("[NHANES 2017-2020](https://wwwn.cdc.gov/nchs/nhanes/continuousnhanes/default.aspx?Cycle=2017-2020)")
+    For more information about NHANES please visit the [NHANES 2017-2020 Website](https://wwwn.cdc.gov/nchs/nhanes/continuousnhanes/default.aspx?Cycle=2017-2020)""")
+    
+
+
+    st.subheader("Project Made By:")
+    st.markdown("[Tobias Madsen](https://www.linkedin.com/in/tob1asmadsen/), Anthony Hernandez, Elle Chen, Jaegun Song, and Kiley Price")
+    st.markdown("*Proud members of [Michigan Data Science Team](https://mdst.club) 🚀*")
+
+
+
 
 
 
